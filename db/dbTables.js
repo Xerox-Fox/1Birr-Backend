acd5633be9d534076c8cf25a5af8232a916997dd
Xerox-Fox <xeroxfox79@gmail.com>
2024-09-16 16:14:31 +0300

    update

---
+++ db/dbTables.js
@@ -0,0 +1,14 @@
+const users = `CREATE TABLE IF NOT EXISTS users (
+    userid INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    bank INT NOT NULL,
+    description TEXT NULL,
+    phone VARCHAR(13),
+    user_type ENUM('user', 'organization') NOT NULL, 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+`;
+
+module.exports = { users };
