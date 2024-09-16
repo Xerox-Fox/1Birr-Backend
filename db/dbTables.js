6a0d47b43ce2986ecb9c9c0183c50d5c73116d1f
Xerox-Fox <xeroxfox79@gmail.com>
2024-09-16 16:26:48 +0300

    update

--- db/dbTables.js
+++ db/dbTables.js
@@ -1,14 +1,15 @@
-const users = `CREATE TABLE users (
-    userid INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    bank INT NOT NULL,
-    description TEXT NULL,
-    phone VARCHAR(13),
-    user_type ENUM('user', 'organization') NOT NULL, 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+const users = `
+    CREATE TABLE IF NOT EXISTS users (
+        userid INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL UNIQUE, -- Added UNIQUE constraint for email
+        password VARCHAR(255) NOT NULL,
+        bank INT NOT NULL,
+        description TEXT NULL,
+        phone VARCHAR(13),
+        user_type ENUM('user', 'organization') NOT NULL, 
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
 `;
 
 module.exports = { users };
